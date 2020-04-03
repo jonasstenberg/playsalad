789f4fedc6db171b9f4258d430e5036e85c8e0ed
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-04-03 22:36:10 +0200

    Add express

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,66 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+const { v4: uuidv4 } = require('uuid')
 const WebSocket = require('ws')
 
-const wss = new WebSocket.Server({ port: 8080 })
+const app = express()
 
-wss.on('connection', function connection (ws) {
-  ws.on('message', function incoming (message) {
-    console.log('received: %s', message)
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+const webSockets = {}
+
+const isProduction = process.env.NODE_ENV === 'production'
+const logFormat = isProduction ? 'tiny' : 'dev'
+
+app.use(bodyParser.json())
+app.use(morgan(logFormat))
+
+wss.on('connection', function connection (ws, req) {
+  const userId = uuidv4()
+  webSockets[userId] = ws
+  ws.isAlive = true
+
+  ws.on('pong', () => {
+    ws.isAlive = true
   })
 
-  ws.send('something')
+  ws.on('message', function incoming (message) {
+    const data = JSON.parse(message)
+    switch (data.action) {
+      case 'createGroup':
+        var r = Math.random().toString(36)
+        var groupId = r.substring(r.length - 4).replace(/0/g, 'o').toUpperCase()
+        webSockets[userId].send(JSON.stringify({
+          groupId
+        }))
+        console.log('creating group')
+        break
+    }
+
+    // send back the message to the other clients
+    wss.clients.forEach(client => {
+      client.send(`Hello, broadcast message -> ${message}`)
+    })
+    console.log('received: %s', data.name)
+  })
+
+  ws.send('Hello! You\'re now connected!')
+})
+
+setInterval(() => {
+  wss.clients.forEach((ws) => {
+    if (!ws.isAlive) {
+      console.log('terminated connection to client')
+      return ws.terminate()
+    }
+
+    ws.isAlive = false
+    ws.ping(null, false, true)
+  })
+}, 5000)
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`Server started on port ${server.address().port}`)
 })
--- client/script.js
+++ client/script.js
@@ -2,7 +2,11 @@ const url = 'ws://localhost:8080'
 const connection = new WebSocket(url)
 
 connection.onopen = () => {
-  connection.send('hey')
+  const player = {
+    action: 'createGroup',
+    name: 'Jonas'
+  }
+  connection.send(JSON.stringify(player))
 }
 
 connection.onerror = (error) => {
