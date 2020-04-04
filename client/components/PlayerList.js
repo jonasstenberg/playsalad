ce0c465c3ff8f0e1f4bbe0774e027006f7020da1
Martin Wendt <martin@blojderfjun.se>
2020-04-04 15:47:24 +0200

    Add styles to player list

--- client/components/PlayerList.js
+++ client/components/PlayerList.js
@@ -2,20 +2,20 @@ import { h } from 'hyperapp'
 
 export default (state, actions) => h('div', { class: 'player-list flex' }, [
   h('div', { class: 'player-list__teams' }, [
-    h('div', { class: 'player-list__team--red' }, [
-      h('div', { class: 'player-list__waiting' }, 'Team red'),
-      h('ul', { class: 'player-list__team' }, [
-        h('li', { class: 'player-list__waiting' }, 'Corona')
+    h('div', { class: 'player-list__team' }, [
+      h('div', { class: 'player-list__team-name player-list__team-name--red' }, 'Team red'),
+      h('ul', { class: 'player-list__players' }, [
+        h('li', { class: 'player-list__player' }, 'Corona')
       ])
     ]),
-    h('div', { class: 'player-list__team--red' }, [
-      h('div', { class: 'player-list__waiting' }, 'Team blue'),
-      h('ul', { class: 'player-list__team' }, [
-        h('li', { class: 'player-list__waiting' }, 'Tegnell')
+    h('div', { class: 'player-list__team' }, [
+      h('div', { class: 'player-list__team-name player-list__team-name--blue' }, 'Team blue'),
+      h('ul', { class: 'player-list__players' }, [
+        h('li', { class: 'player-list__player' }, 'Tegnell')
       ])
     ])
   ]),
   h('div', { class: 'player-list__waiting' }, 'waiting for other players to join...'),
-  h('button', { class: 'button button--blue' }, 'Submit names'),
+  h('button', { class: 'button button--blue' }, 'Throw in names'),
   h('button', { class: 'button button--orange' }, 'Start game')
 ])
