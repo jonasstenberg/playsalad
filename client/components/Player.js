a7c9a16fa43e33d8e947d2fd1014b1c8cc1a55dd
Martin Wendt <martin@blojderfjun.se>
2020-04-30 21:01:12 +0200

    Add indicator to the game host in player list

--- client/components/Player.js
+++ client/components/Player.js
@@ -1,11 +1,16 @@
 import { h } from 'hyperapp'
 
 export default (state, playerId) => h('li', {
-  class: `player-list__player${playerId === state.playerId ? ' player-list__player--current' : ''}`
+  class: `player-list__player
+    ${playerId === state.playerId ? ' player-list__player--current' : ''}
+    ${playerId === state.room.ownerId ? ' player-list__player--host' : ''}`
 }, [
   state.room.players[playerId].notes ? h('img', {
     src: '/images/checkmark.svg',
     class: 'player-list__player--check'
   }) : '',
-  h('span', {}, state.room.players[playerId].name)
+  h('span', {}, state.room.players[playerId].name),
+  playerId === state.room.ownerId
+    ? h('img', { src: '/images/chef.svg', class: 'player-list__host-image' })
+    : null
 ])
