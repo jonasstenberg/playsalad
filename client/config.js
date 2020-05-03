3ce2d9eee3ac258137296d2ce54cc8c0fa6940a2
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-05-03 16:51:41 +0200

    Fix client config

--- client/config.js
+++ client/config.js
@@ -1,13 +1,5 @@
-if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-  module.exports = {
-    websocketUrl: 'ws://localhost:8080/ws',
-    backendBaseUrl: 'http://localhost:8080/api',
-    timeout: 10
-  }
-} else {
-  module.exports = {
-    websocketUrl: `wss://${window.location.hostname}/ws`,
-    backendBaseUrl: '/api',
-    timeout: 60
-  }
+module.exports = {
+  websocketUrl: `wss://${window.location.hostname}/ws`,
+  backendBaseUrl: '/api',
+  timeout: 60
 }
