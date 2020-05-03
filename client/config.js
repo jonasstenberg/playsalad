55b9c2abf0c442ec41a8fe89bbce15d15caa607b
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-05-03 16:53:56 +0200

    Revert "Fix client config"
    This reverts commit 3ce2d9eee3ac258137296d2ce54cc8c0fa6940a2.

--- client/config.js
+++ client/config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  websocketUrl: `wss://${window.location.hostname}/ws`,
-  backendBaseUrl: '/api',
-  timeout: 60
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  module.exports = {
+    websocketUrl: 'ws://localhost:8080/ws',
+    backendBaseUrl: 'http://localhost:8080/api',
+    timeout: 10
+  }
+} else {
+  module.exports = {
+    websocketUrl: `wss://${window.location.hostname}/ws`,
+    backendBaseUrl: '/api',
+    timeout: 60
+  }
 }
