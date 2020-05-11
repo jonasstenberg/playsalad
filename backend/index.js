25b091b5ee5dc275f1813b97eb47a95db263fc59
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-05-11 21:47:55 +0200

    Add Content-Security-Policy header

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,11 @@ setInterval(() => {
   })
 }, 5000)
 
+app.use((req, res, next) => {
+  res.header('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none'; img-src 'self'; media-src 'self'; frame-src 'none'; font-src 'self' https://fonts.gstatic.com data:; connect-src 'self'; style-src 'self'")
+  next()
+})
+
 app.use('/api/rooms', require('./routes/rooms'))
 app.use('/api/players', require('./routes/players'))
 app.use('/api/games', require('./routes/games'))
