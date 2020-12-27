bde77919ac82805c6021fa2be11584573e877592
Jonas Stenberg <github@stenberg.io>
2020-12-27 22:39:08 +0100

    Update deploy flow

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
   module.exports = {
-    websocketUrl: 'ws://localhost:8080/ws',
-    backendBaseUrl: 'http://localhost:8080/api',
+    websocketUrl: 'ws://localhost:8081/ws',
+    backendBaseUrl: 'http://localhost:8081/api',
     timeout: 60,
     debug: true
   }
